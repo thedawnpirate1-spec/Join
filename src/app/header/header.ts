89c29d7aebc7076b1d86f1f418c61a3c3ab7d7fc
Leon Lammerich <thedawnpirate1@gmail.com>
2026-07-02 15:56:42 +0200

    feat header/fonts

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  isMenuOpen = false;
+
+  toggleMenu(event: Event) {
+    event.stopPropagation();
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('document:click')
+  onDocumentClick() {
+    this.closeMenu();
+  }
+}
