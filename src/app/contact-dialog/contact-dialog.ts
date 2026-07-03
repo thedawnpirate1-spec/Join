8453c66507eb327ea55715f256d78e7ef0be557c
eddyfr3se <chrisunder@gmx.de>
2026-07-03 13:52:05 +0200

    navbar-icons

---
+++ src/app/contact-dialog/contact-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-dialog',
+  imports: [],
+  templateUrl: './contact-dialog.html',
+  styleUrl: './contact-dialog.scss',
+})
+export class ContactDialog {}
