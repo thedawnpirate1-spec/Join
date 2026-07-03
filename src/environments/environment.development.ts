ffd4cf091b9009222edb47fcc01ca696b517901d
Sahin Varol <sahin@varol-dev.com>
2026-07-03 12:40:01 +0200

    deleted environments folder and added it to gitignore

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://zjfkaeqvbjguawrjkvmw.supabase.co',
+  supabaseUrl: 'https://zjfkaeqvbjguawrjkvmw.supabase.co/',
   supabaseAnonKey: 'sb_publishable_WKjP_ShEERXo7_aH9unf8A_QrmA0_A-',
 };
