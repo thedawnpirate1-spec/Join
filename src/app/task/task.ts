d32bf99cd95456711db2ca227b15b8f34b0b18bf
Sahin Varol <sahin@varol-dev.com>
2026-07-10 12:11:09 +0200

    created task component

---
+++ src/app/task/task.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.html',
+  styleUrl: './task.scss',
+})
+export class Task {}
