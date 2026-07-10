c58674cdbfcd23055abafce92a5c305bd05da85b
Sahin Varol <sahin@varol-dev.com>
2026-07-10 16:37:43 +0200

    created template file for environment

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,11 @@
+//Create two new files called:
+// environment.development.ts and
+// environment.ts
+// in the src/environments directory.
+// This file will serve as a template for the environment configuration.
+// It should contain the following code:
+export const environment = {
+  production: false,
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+};
