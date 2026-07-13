4eff72da975aff962eb85a7740cb5b3dd4387273
Sahin Varol <sahin@varol-dev.com>
2026-07-13 16:23:40 +0200

    created js doc for task.ts

--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -47,30 +47,59 @@ export class Task implements OnChanges {
     }
   }
 
+  /**
+   * Returns the readable label for the task category.
+   */
   get categoryLabel(): string {
     return this.task.category === 'technical_task' ? 'Technical Task' : 'User Story';
   }
 
+  /**
+   * Returns the number of completed subtasks.
+   */
   get completedSubtaskCount(): number {
     return this.subtasks.filter((subtask) => subtask.done).length;
   }
 
+  /**
+   * Returns the completion percentage of all subtasks.
+   * Used for the width of the subtask progress bar.
+   */
   get subtaskProgressPercent(): number {
     return this.subtasks.length ? (this.completedSubtaskCount / this.subtasks.length) * 100 : 0;
   }
 
+  /**
+   * Returns the assigned contacts that should be displayed as avatar circles.
+   * The task card shows a maximum of three avatars directly.
+   */
   get visibleAssignedContacts(): Contact[] {
     return this.assignedContacts.slice(0, 3);
   }
 
+  /**
+   * Returns the number of assigned contacts that are hidden behind the "+x" avatar.
+   */
   get hiddenAssignedContactsCount(): number {
     return Math.max(this.assignedContacts.length - 3, 0);
   }
 
+  /**
+   * Returns the initials that should be displayed inside a contact avatar.
+   *
+   * @param contact - The contact whose initials should be displayed.
+   * @returns The contact initials.
+   */
   getAvatarInitials(contact: Contact): string {
     return getContactInitials(contact);
   }
 
+  /**
+   * Returns the background color that should be used for a contact avatar.
+   *
+   * @param contact - The contact whose avatar color should be returned.
+   * @returns The avatar background color as a hex color string.
+   */
   getAvatarColor(contact: Contact): string {
     return getContactColor(contact);
   }
