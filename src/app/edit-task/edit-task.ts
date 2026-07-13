aa97d421e5e3a3e762358ee4de0207bfdcece193
Sahin Varol <sahin@varol-dev.com>
2026-07-13 12:21:19 +0200

    created edit-task component

---
+++ src/app/edit-task/edit-task.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-task',
+  imports: [],
+  templateUrl: './edit-task.html',
+  styleUrl: './edit-task.scss',
+})
+export class EditTask {}
