640a1580f728a4bef4f03afa1a1ea4e7d630093c
Leon Lammerich <thedawnpirate1@gmail.com>
2026-07-14 13:56:28 +0200

    fixed repsonisve tablet navbar / view

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,52 +1,12 @@
-import { Component, ElementRef, HostListener, Input, OnInit, inject } from '@angular/core';
-import { Router, NavigationEnd, RouterLink } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar implements OnInit {
+export class Navbar {
   @Input() isLoggedIn: boolean = true;
-  activeLink: string = 'Summary';
-  private router = inject(Router);
-
-  constructor(private elementRef: ElementRef) {
-    this.router.events.pipe(
-      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
-    ).subscribe((event) => {
-      this.updateActiveLink(event.urlAfterRedirects);
-    });
-  }
-
-  ngOnInit() {
-    this.updateActiveLink(this.router.url);
-  }
-
-  updateActiveLink(url: string) {
-    if (url.includes('/contacts')) {
-      this.activeLink = 'Contacts';
-    } else if (url.includes('/board')) {
-      this.activeLink = 'Board';
-    } else if (url.includes('/add-task')) {
-      this.activeLink = 'Add Task';
-    } else if (url === '/' || url.includes('/summary')) {
-      this.activeLink = 'Summary';
-    } else {
-      this.activeLink = '';
-    }
-  }
-
-  setActiveLink(link: string): void {
-    this.activeLink = link;
-  }
-
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: Event): void {
-    if (!this.elementRef.nativeElement.contains(event.target)) {
-      this.updateActiveLink(this.router.url);
-    }
-  }
 }
