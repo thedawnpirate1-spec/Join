a0490c3f1380149d7f9d41fa83559e358384ac5b
Sahin Varol <sahin@varol-dev.com>
2026-07-15 13:12:55 +0200

    moved code from mainside component to summary.

--- src/app/summary/summary.ts
+++ src/app/summary/summary.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './summary.html',
   styleUrl: './summary.scss',
 })
-export class Summary {}
+export class Summary {
+  todoCount = 1;
+  doneCount = 1;
+  urgentCount = 1;
+  boardCount = 5;
+  progressCount = 2;
+  feedbackCount = 2;
+  upcomingDeadline = 'October 16, 2026';
+  userName = 'Sofia Müller';
+}
