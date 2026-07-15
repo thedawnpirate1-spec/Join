97f2435801dc0dee22102d523a1dfa0ad137d5d3
Leon Lammerich <thedawnpirate1@gmail.com>
2026-07-15 11:39:28 +0200

    feat: add mainside dashboard component with metrics overview and greeting section

--- src/app/mainside/mainside.ts
+++ src/app/mainside/mainside.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './mainside.html',
   styleUrl: './mainside.scss',
 })
-export class Mainside {}
+export class Mainside {
+  todoCount = 1;
+  doneCount = 1;
+  urgentCount = 1;
+  boardCount = 5;
+  progressCount = 2;
+  feedbackCount = 2;
+  upcomingDeadline = 'October 16, 2026';
+  userName = 'Sofia Müller';
+}
